4b0662ec3ac0b565e86eb68b34783ffb407107f6
Agnieszka Florkowska <aga.florkowska99@gmail.com>
2023-01-17 19:44:53 +0100

    ECG - Baseline - Improved MA Filter and Butterworth Filter

--- Modules/ECG-Baseline/ECG_Baseline.h
+++ Modules/ECG-Baseline/ECG_Baseline.h
@@ -9,6 +9,7 @@
 #include <utility>
 #include <numeric>
 #define M_PIl 3.141592653589793238462643383279502884L
+#define M_PI 3.14159265358979323846
 
 enum ConvolutionType
 {
@@ -22,15 +23,18 @@ class MovingAverageFilterParameters
 public:
 	MovingAverageFilterParameters();
 
-	void SetWindowLenght(unsigned int length);
-	unsigned int GetWindowLenght();
-	std::vector<float> GetConvolutionWindow();
+	void SetWindowLenghtLow(unsigned int length);
+	void SetWindowLenghtHigh(unsigned int length);
+	std::vector<float> GetWindowLow();
+	std::vector<float> GetWindowHigh();
 	void ResetConvolutionWindow();
 	void CreateMAFConvolutionWindow();
 
 private:
-	unsigned int m_windowLength;
-	std::vector<float> m_convolutionWindow;
+	unsigned int m_windowLengthLow;
+	unsigned int m_windowLengthHigh;
+	std::vector<float> m_convolutionWindowLow;
+	std::vector<float> m_convolutionWindowHigh;
 };
 
 class LMSFilterParameters
@@ -58,7 +62,7 @@ class ButterworthFilterParameters
 public:
 	ButterworthFilterParameters();
 
-	void CreateBWBandPassFilter();
+	std::vector<float> CalculateDifferenceEquation(std::shared_ptr<std::vector<float>> signal, std::vector<float> coeff);
 
 	unsigned int GetBWFrequencyLowPassFilter();
 	void SetBWFrequencyLowPassFilter(unsigned int freq);
@@ -66,22 +70,24 @@ public:
 	void SetBWFrequencyHighPassFilter(unsigned int freq);
 	unsigned int GetBWFrequencySampling();
 	void SetBWFrequencySampling(unsigned int freq);
-	unsigned int GetBWCoefficientsNumber();
-	void SetBWCoefficientsNumber(unsigned int number);
 
-	std::vector<float> GetLowPassFilter();
-	std::vector<float> GetHighPassFilter();
+	void CalculateFilterCoeff();
+	std::vector<float> GetLowPassFilterCoeff();
+	std::vector<float> GetHighPassFilterCoeff();
 
 	void ResetFilterParams();
 
 private:
+
+	void CalculateLowPassFilterCoeff();
+	void CalculateHighPassFilterCoeff();
+
 	unsigned int m_fLowPass;
 	unsigned int m_fHighPass;
 	unsigned int m_fSampling;
-	unsigned int m_coefficientsNumber;
-	std::vector<float> m_lowPassFilter;
-	std::vector<float> m_highPassFilter;
-	std::vector<float> m_hammingWindow;
+	std::vector<float> m_lowPassFilterCoeff;
+	std::vector<float> m_highPassFilterCoeff;
+	
 };
 
 // This module enables to remove the baseline wander from ECG signals.
@@ -91,15 +97,16 @@ class ECG_Baseline
 	public:
 		ECG_Baseline(std::shared_ptr<std::vector<float>> originalSignal);
 
-		// In the case of Butterworth filtering's method, the user should enter the low pass frequency, high pass frequency, and number of coefficients.
+		// In the case of Butterworth filtering's method, the user should enter the low pass frequency, high pass frequency.
 		// Moreover, the sampling frequency should be set to loaded value from signal properties. 
 		void SetSamplingFrequencyForButteworthFilter(unsigned int freq);
 		void SetLowPassFrequencyForButteworthFilter(unsigned int freq);
 		void SetHighPassFrequencyForButteworthFilter(unsigned int freq);
-		void SetNumberOfCoefficientsForButteworthFilter(unsigned int number);
 
 		// In the case of Moving Average filtering's method, the user should enter the window length.
-		void SetWindowLengthForMovingAverageFilter(unsigned int length);
+		void SetWindowLengthForMovingAverageFilter(unsigned int lengthLow, unsigned int lengthHigh);
+		// Input:
+		//			lengthLow > lengthHigh
 
 		// In the case of LMS filtering's method, the user should enter the convergance rate and the window length for Moving Average Filter.
 		void SetConvergenceRateForLMSFilter(float rate);
@@ -111,11 +118,10 @@ class ECG_Baseline
 		// Input: 
 		//		std::shared_ptr<std::vector<float>> m_originalSignal != nullptr
 		//		ButterworthFilterParameters 
-		//			unsigned int m_fLowPass > 0 (prefered value =  15)
-		//			unsigned int m_fHighPass > 0 (prefered value =  5)
+		//			unsigned int m_fLowPass > 0 (default value =  15)
+		//			unsigned int m_fHighPass > 0 (default value =  1)
 		//			m_fLowPass > m_fHighPass
 		//			unsigned int m_fSampling > 0
-		//			unsigned int m_coefficientsNumber > 0 (prefered value =  40)
 
 		// 
 		// Output:
@@ -125,7 +131,8 @@ class ECG_Baseline
 		// Input: 
 		//		std::shared_ptr<std::vector<float>> m_originalSignal != nullptr
 		//		MovingAverageFilterParameters 
-		//			unsigned int m_windowLength > 0 (prefered value =  15)
+		//			unsigned int m_windowLengthLow > 0 (default value =  15)
+		//			unsigned int m_windowLengthHigh > 0 (default value =  5)
 		//
 		// Output:
 		//		std::vector<float> m_filteredSignalUsingMovingAverageFilter 
@@ -134,10 +141,11 @@ class ECG_Baseline
 		// Input: 
 		//		std::shared_ptr<std::vector<float>> m_originalSignal != nullptr
 		//		LMSFilterParameters 
-		//			float m_convergenceRate > 0 (prefered value =  0.02)
+		//			float m_convergenceRate > 0 (default value =  0.02)
 		// 
 		//		MovingAverageFilterParameters - needed for calculating the desired signal during LMS filtering
-		//			unsigned int m_windowLength > 0 (prefered value =  15)
+		//			unsigned int m_windowLengthLow > 0 (default value =  15)
+		//			unsigned int m_windowLengthHigh > 0 (default value =  5)
 		//
 		// Output:
 		//		std::vector<float> m_filteredSignalUsingLMSFilter 
