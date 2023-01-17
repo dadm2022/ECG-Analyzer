7c7a226a9de2780614a029051012bd1815b9e99b
Maria Nowak <marysian52@gmail.com>
2023-01-17 22:05:09 +0100

    [HRV1] - [calosc]

--- Modules/HRV1/HRV1.h
+++ Modules/HRV1/HRV1.h
@@ -5,12 +5,19 @@
 #ifndef ECG_ANALYZER_HRV1_H
 #define ECG_ANALYZER_HRV1_H
 
-#include <cmath>
-#include <vector>
 #include <iostream>
-#include <fstream>
+#include <vector>
 
+/*
+    Example usage:
+    std::shared_ptr<const std::vector<int>> RR_Peaks
+    HRV1 hrv1(RR_Peaks); - creating class instance
+    hrv1.calculateParams(); - calculating statistic and frequency parameters
+
+    To get parameters use proper getter, for example to get meanRR use Get_meanRR();
+ */
 using namespace std;
+
 
 struct HRV1_results {
     float meanRR;
@@ -31,7 +38,7 @@ struct HRV1_results {
 class HRV1 {
 
 public:
-    HRV1(std::vector<int> RR_peaks) : m_RR_peaks(RR_peaks){}; // add later as std::shared_ptr
+    HRV1(std::vector<int> RR_peaks) : m_RR_peaks(RR_peaks){};
 
     void calculateParams();
     float Get_meanRR();
@@ -39,19 +46,35 @@ public:
     float Get_valueRMSSD();
     unsigned int Get_valueNN50();
     float Get_valuePNN50();
+    float Get_HF();
+    float Get_LF();
+    float Get_VLF();
+    float Get_ULF();
+    float Get_TP();
+    float Get_LFHF();
 
 private:
+
+    vector<float> m_vectorRPeaks;
+    std::shared_ptr<std::vector<int>> m_RR_peaks;
+    HRV1_results m_Results;
+
     void calculateStatisticParams();
     void calculateFrequencyParams();
+
     vector<float> createTachogram();
     float calculateMeanRR();
     float calculateSDNN();
     float calculateRMSSD();
     unsigned int calculateNN50();
     float calculatePNN50();
-    vector<float> m_vectorRPeaks;
-    HRV1_results m_Results;
-    vector<int> m_RR_peaks // add later as shared ptr
+
+    vector<float> prepareSigAbsolute();
+    float getSigLen(vector<float> vectorRPeaks);
+    vector<float> calculatePSD(const vector<float> sigSpline);
+    vector<float> calculateFreqSpect(const vector<float> sigSpline);
+    vector<float> cubicSpline();
+
 };
 
 #endif //ECG_ANALYZER_HRV1_H
