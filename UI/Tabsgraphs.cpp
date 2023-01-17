213a2ae689ba68e8264a3ab04c2eb7b2300cd566
PiotrPietranis <piotrekpietranis@gmail.com>
2023-01-18 00:12:44 +0100

    HRV2 chart callout alfa

--- UI/Tabsgraphs.cpp
+++ UI/Tabsgraphs.cpp
@@ -6,6 +6,7 @@
 #include "chartCallout.h"
 #include "Visualization/ECG_chartCallout.h"
 #include "Visualization/Peaks_chartCallout.h"
+#include "Visualization/HRV2_chartCallout.h"
 
 
 
@@ -67,7 +68,9 @@ void Tabsgraphs::addtab_RPeaksDetectionPanTompkins(){
         std::vector <int> waves5 = {1, 2, 3, 4, 5, 11, 14, 16};
         std::vector <int> waves6 = {1, 2, 3, 4, 5, 10, 14, 15};
         std::vector <int> waves7 = {3, 5, 7, 9, 10, 11, 14, 16};
+
         auto *obj = new PeaksChartCallout(filteredSignal, detectedRPeaks, waves1, waves2, waves3, waves4, waves5, waves6, waves7);
+       // test histogram
         QWidget *tab1 = new QWidget;
         this->addTab(tab1, "QRSPT - Pan Tompkins");
 
@@ -77,13 +80,28 @@ void Tabsgraphs::addtab_RPeaksDetectionPanTompkins(){
 };
 
 void Tabsgraphs::addtab_RPeaksDetectionHilbert(){
+
+    std::vector <float> filteredSignal={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    std::vector <int> detectedRPeaks={2,4,6,7,8,10,14,16,20};
+    std::vector <int> waves1 = {1, 2, 3, 4, 5, 7, 15, 17};
+    std::vector <int> waves2 = {1, 2, 4, 4, 5, 8, 15, 18};
+    std::vector <int> waves3 = {1, 2, 3, 4, 5, 9, 15, 19};
+    std::vector <int> waves4 = {1, 2, 3, 4, 5, 10, 14, 15};
+    std::vector <int> waves5 = {1, 2, 3, 4, 5, 11, 14, 16};
+    std::vector <int> waves6 = {1, 2, 3, 4, 5, 10, 14, 15};
+    std::vector <int> waves7 = {3, 5, 7, 9, 10, 11, 14, 16};
+
+    auto *obj = new PeaksChartCallout(filteredSignal, detectedRPeaks, waves1, waves2, waves3, waves4, waves5, waves6, waves7);
+
     QWidget *tab1 = new QWidget;
     this->addTab(tab1, "QRSPT - Hilbert");
     QVBoxLayout *vlayout = new QVBoxLayout;
+    vlayout->addWidget(obj);
     tab1->setLayout(vlayout);
 };
 
 void Tabsgraphs::addtab_QRSandPTDetection(){
+
     QWidget *tab1 = new QWidget;
     this->addTab(tab1, "QRS and PT Detection");
     QVBoxLayout *vlayout = new QVBoxLayout;
@@ -91,9 +109,23 @@ void Tabsgraphs::addtab_QRSandPTDetection(){
 };
 
 void Tabsgraphs::addtab_HRV2(){
+
+    //histogram mock
+    std::vector<int> bins = {6,7,8};
+    std::vector<int> dupa = {1,2,3};
+    int xM =5;
+    int yM =5;
+    int xT =7;
+    int yT =10;
+    int xN =7;
+    int yN =5;
+
+    auto *obj = new HRV2HistogramCallout(bins, dupa, xM, yM, xT, yT, xN, yN);
+
     QWidget *tab1 = new QWidget;
     this->addTab(tab1, "HRV2");
     QVBoxLayout *vlayout = new QVBoxLayout;
+    vlayout->addWidget(obj);
     tab1->setLayout(vlayout);
 };
 
