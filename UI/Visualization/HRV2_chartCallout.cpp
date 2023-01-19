c9b3f99089ab3d7e16596b791d985974b307ed65
PiotrPietranis <piotrekpietranis@gmail.com>
2023-01-19 03:52:01 +0100

    HRV2 and SDPointcare chart callouts

--- UI/Visualization/HRV2_chartCallout.cpp
+++ UI/Visualization/HRV2_chartCallout.cpp
@@ -1,5 +1,5 @@
 //
-// Created by Weronika on 16.01.2023.
+// Created by Weronika Wójcik and Piotr Pietranis on 16.01.2023.
 //
 
 #include <QtWidgets/QApplication>
@@ -12,31 +12,22 @@
 #include <QtCharts/QBarCategoryAxis>
 #include <QtCharts/QValueAxis>
 #include "HRV2_chartCallout.h"
+#include "SignalUtils.h"
 #include<QLineF>
+
 #include "../../Modules/HRV2/HRV2.h"
 
-//HRV2HistogramCallout(std::shared_ptr<HRV2>histogram);
-//HRV2HistogramCallout(std::vector<int> HRV2::peakDistances,
-//                     std::vector<int> HRV2::poincareX,
-//                     std::vector<int> HRV2::poincareY,
-//                     std::vector<int> HRV2::identityLine,
-//                     std::vector<int> HistogramData::histogram,
-//                     std::vector<double> HistogramData::binBorders,
-//                     std::vector<int> HistogramData::maxBinSize,
-//                     std::vector<int> HistogramData::maxBinNumber,
-//                     std::vector<double> HRV2::TinnParams::xVectorMT,
-//                     std::vector<double> HRV2::TinnParams::yVectorMT,
-//                     std::vector<double> HRV2::triangularIndex,
-//                     std::vector<int> EllipseParameters::indicesSD1,
-//                     std::vector<int> EllipseParameters::valuesSD1);
-//HRV2HistogramCallout(std::vector<double> binsBorders, std::vector<int> histogram, int maxHistogramValueIndex,
-//                     std::shared_ptr<HRV2> hrv2);
-HRV2HistogramCallout::HRV2HistogramCallout(std::vector<int> &bins, std::vector<int> &dupa,
-                                           int&xM, int&yM, int &xT, int &yT,  int &xN, int &yN):
+
+using SignalUtils::getRangeFromTo;
+
+HRV2HistogramCallout::HRV2HistogramCallout(int &bins, std::vector<float> &highBins, std::vector<float> &binsCenter,
+                                           float &binsWidth, int&xM, int&yM, int &xT, int &yT, int &xN, int &yN):
         QtCharts::QChartView(){
 
-        this->dupa = dupa;
         this->bins = bins;
+        this->highBins = highBins;
+        this->binsCenter = binsCenter;
+        this->binsWidth = binsWidth;
         this->xT = xT;
         this->yT = yT;
         this->xM = xM;
@@ -44,33 +35,19 @@ HRV2HistogramCallout::HRV2HistogramCallout(std::vector<int> &bins, std::vector<i
         this->xN = xN;
         this->yN = yN;
 
-//    this->peakDistances = peakDistances;
-//    this->poincareX = poincareX;
-//    this->poincareY = poincareY;
-//    this->identityLine = identityLine;
-//    this->histogram = histogram;
-//    this->binBorders = binBorders;
-//    this->maxBinSize = maxBinSize;
-//    this->maxBinNumber = maxBinNumber;
-//    this->xVectorMT = xVectorMT;
-//    this->yVectorMT = yVectorMT;
-//    this->xVectorMT = xVectorMN;
-//    this->yVectorMT = yVectorMN;
-//    this->xVectorMT = xVectorTN;
-//    this->yVectorMT = yVectorTN;
-//    this->triangularIndex = triangularIndex;
-//    this->indicesSD1 = indicesSD1;
-//    this->valuesSD1 = valuesSD1;
 
     createSeries();
     addDataToBarQSeries(0);
     chart = new QtCharts::QChart();
     chart->setAcceptHoverEvents(true);
 
+    auto [fromX, toX] = getRangeFromTo(binsCenter);
+    auto [fromY, toY] = getRangeFromTo(highBins);
+
     defXaxis = new QValueAxis();
     defYaxis = new QValueAxis();
-    defXaxis->setRange(0, 30);
-    defYaxis->setRange(0, 30);
+    defXaxis->setRange(0, toX + 50);
+    defYaxis->setRange(0, toY + 50);
 
     chart->addAxis(defXaxis, Qt::AlignBottom);
     chart->addAxis(defYaxis, Qt::AlignLeft);
@@ -81,12 +58,14 @@ HRV2HistogramCallout::HRV2HistogramCallout(std::vector<int> &bins, std::vector<i
     font.setPixelSize(18);
     chart->setTitleFont(font);
     chart->setTitleBrush(Qt::black);
-    chart->setTitle("histogram");
+    chart->setTitle("Histogram interwałów RR i indeks trójkątny");
 
     auto *chartView = new QChartView(chart);
     auto *mainLayout = new QVBoxLayout;
 
     mainLayout->addWidget(chartView);
+    chart->axes(Qt::Horizontal).back()->setTitleText("Wartości przedziałów [ms]");
+    chart->axes(Qt::Vertical).back()->setTitleText("Liczba zliczeń");
     setLayout(mainLayout);
     setRenderHint(QPainter::Antialiasing);
 
@@ -98,6 +77,8 @@ HRV2HistogramCallout::HRV2HistogramCallout(std::vector<int> &bins, std::vector<i
 void HRV2HistogramCallout::createSeries() {
 
     lineseries = new QtCharts::QLineSeries;
+    linehistogram = new QtCharts::QLineSeries;
+
 
     ParamM = new QtCharts::QScatterSeries();
     ParamT = new QtCharts::QScatterSeries();
@@ -119,10 +100,6 @@ void HRV2HistogramCallout::createSeries() {
     ParamN->setColor(QRgb(0x9900FF));
 
 
-    barseries = new QtCharts::QBarSeries;
-    set = new QtCharts::QBarSet("BINS");
-    barseries->setName("dane bar");
-
     QPen pen(QRgb(0x0000FF));
     pen.setWidth(1);
     lineseries->setPen(pen);
@@ -132,13 +109,21 @@ void HRV2HistogramCallout::createSeries() {
 void HRV2HistogramCallout::addDataToBarQSeries(int startIndex) {
 
 
-    int signalValue;
+    for(int i = 0; i < bins; i++){
 
-//    for (int i = startIndex; xM - xT; i++) {
-//
-//        signalValue = dupa[i];
-//        lineseries->append(i, signalValue);
-//    }
+        double Ax = (binsCenter[i] - 0.5*binsWidth);
+        double Bx = Ax;
+        double By = highBins[i];
+        double Cx = (binsCenter[i] + 0.5*binsWidth);
+        double Cy = By;
+        double Dx = Cx;
+        linehistogram->append(Ax, 0);
+        linehistogram->append(Bx, By);
+        linehistogram->append(Cx, Cy);
+        linehistogram->append(Dx, 0);
+
+    }
+
     lineseries->append(xM,yM);
     lineseries->append(xN,yN);
     lineseries->append(xT, yT);
@@ -148,32 +133,20 @@ void HRV2HistogramCallout::addDataToBarQSeries(int startIndex) {
     ParamT->append(xT, yT);
     ParamN->append(xN, yN);
 
-
-    int barValue;
-
-    for (int i = startIndex; i < startIndex + 25; i++) {
-        if (i >= bins.size()) {
-            break;
-        }
-        barValue = bins[i];
-        set->append(barValue);
-    }
-    barseries->append(set);
-
 }
 
 void HRV2HistogramCallout::addSeriesToChart() {
 
-    chart->addSeries(barseries);
+//    chart->addSeries(barseries);
+    chart->addSeries(linehistogram);
     chart->addSeries(lineseries);
 
     chart->addSeries(ParamM);
     chart->addSeries(ParamT);
     chart->addSeries(ParamN);
 
-
-    chart->setTitle("Line and barchart example");
-
+    linehistogram->attachAxis(defXaxis);
+    linehistogram->attachAxis(defYaxis);
 
     lineseries->attachAxis(defXaxis);
     lineseries->attachAxis(defYaxis);
@@ -187,51 +160,4 @@ void HRV2HistogramCallout::addSeriesToChart() {
     ParamN->attachAxis(defXaxis);
     ParamN->attachAxis(defYaxis);
 
-    QStringList label;
-    label << "X";
-    axisX = new QBarCategoryAxis();
-    axisX->append(label);
-    barseries->attachAxis(axisX);
 }
-
-
-
-//    auto histogramHRV2 = HistogramData::histogram;
-//    HistogramData::histogram
-//
-//    this->peakDistances =
-//
-//    this->HistogramData->peakDistances;
-//    std::vector<int> histogram = histogramHRV2->histogram;
-
- //   auto abc = GetHistogram (bins);
-//    int bins = 16;
-//    hrv2.SetHistogram(bins);
-//    HistogramData histogramData = hrv2.GetHistogram();
-//    std::vector <int> histogram = histogramData.histogram;
-//    std::vector<double> binBorders = histogramData.binBorders;
-//    double interval = histogramData.interval;
-//    int maxBinSize = histogramData.maxBinSize;
-//    int maxBinNumber = histogramData.maxBinNumber;
-//    std::vector<double> binCenters = hrv2.calcBinCenters(bins, interval, binBorders);
-//
-//    hrv2.setTinn(maxBinNumber, maxBinSize, bins, binBorders, binCenters, histogram);
-//    TinnParams tinnParams = hrv2.getTinn();
-//    std::vector<double> xVectorMT = tinnParams.xVectorMT;
-//    std::vector<double> yVectorMT = tinnParams.yVectorMT;
-//    double TinnLength = tinnParams.TinnLength;
-//
-//    hrv2.setTriangularIndex();
-//    hrv2.getTriangularIndex();
-//    double triangularIndex = hrv2.triangularIndex;
-//
-//    PoincareParameters poincareParameters = hrv2.GetPoincareParameters();
-//    double sd1 = poincareParameters.sd1;
-//    std::vector<int> poincareX = hrv2.getPoincareX();
-//    std::vector<int> poincareY = hrv2.getPoincareY();
-//    std::vector<int> identityLine = hrv2.GetIdentityLine();
-//
-//    EllipseParameters ellipseParameters = hrv2.GetEllipse();
-//    std::vector<int> indicesSD1 = ellipseParameters.indicesSD1;
-//    std::vector<int> valuesSD1 = ellipseParameters.valuesSD1;
-
--- UI/Visualization/HRV2_chartCallout.h
+++ UI/Visualization/HRV2_chartCallout.h
@@ -1,10 +1,12 @@
 //
-// Created by Weronika on 16.01.2023.
+// Created by Weronika Wójcik and Piotr Pietranis on 16.01.2023.
 //
 
 #ifndef ECG_ANALYZER_HRV2_CHARTCALLOUT_H
 #define ECG_ANALYZER_HRV2_CHARTCALLOUT_H
 #include <QWidget>
+#include <QPainter>
+#include <QRectF>
 #include <QtCharts>
 #include <QDialog>
 #include <QLineF>
@@ -18,85 +20,27 @@ class HRV2HistogramCallout : public QChartView {
     Q_OBJECT
 
 public:
-//   // explicit HRV2HistogramCallout(std::shared_ptr<HRV2>histogram);
-//   explicit HRV2HistogramCallout(std::vector<int> &peakDistances,
-//                                 std::vector<int> &poincareX,
-//                                 std::vector<int> &poincareY,
-//                                 std::vector<int> &identityLine,
-//                                 std::vector<int> &histogram,
-//                                 std::vector<double> &binBorders,
-//                                 std::vector<int> &maxBinSize,
-//                                 std::vector<int> &maxBinNumber,
-//                                 std::vector<double> &xVectorMT,
-//                                 std::vector<double> &yVectorMT,
-//                                 std::vector<double> &triangularIndex,
-//                                 std::vector<int> &indicesSD1,
-//                                 std::vector<int> &valuesSD1);
 
-//
-//    explicit HRV2HistogramCallout(std::vector<double> binsBorders, std::vector<int> histogram, int maxHistogramValueIndex,
-//                                  const std::shared_ptr<HRV2> hrv2Ptr,
-//                                  );
 
-//    std::vector<int> &peakDistances,
-//    std::vector<int> &poincareX,
-//    std::vector<int> &poincareY,
-//    std::vector<int> &identityLine,
-//    std::vector<int> &histogram,
-//    std::vector<double> &binBorders,
-//    std::vector<int> &maxBinSize,
-//    std::vector<int> &maxBinNumber,
-//    std::vector<double> &xVectorMT,
-//    std::vector<double> &yVectorMT,
-//    std::vector<double> &xVectorMN,
-//    std::vector<double> &yVectorMN,
-//    std::vector<double> &xVectorTN,
-//    std::vector<double> &yVectorTN,
-//    std::vector<double> &triangularIndex,
-//    std::vector<int> &indicesSD1,
-//    std::vector<int> &valuesSD1
-
-    explicit HRV2HistogramCallout(std::vector<int> &bins, std::vector<int> &dupa,
-                                  int &xT, int &yT, int&xM, int&yM, int &xN, int &yN);
+    explicit HRV2HistogramCallout(int &bins, std::vector<float> &highBins, std::vector<float> &binsCenter,
+                                  float &binsWidth, int&xM, int&yM, int &xT, int &yT, int &xN, int &yN);
 
 private :
 
-    //std::shared_ptr<HRV2> HistogramData;
-    QScrollBar *scrollbar;
     QChart *chart;
-    QBarSeries *barseries;
     QLineSeries *lineseries;
-    QLineSeries *lineseriesMT;
-    QLineSeries *lineseriesTN;
-    QLineSeries *lineseriesMN;
+    QLineSeries *linehistogram;
     QValueAxis *defXaxis;
     QValueAxis *defYaxis;
     QValueAxis *defXTimeAxis;
-    QBarSet *set;
-    QBarCategoryAxis *axisX;
     QScatterSeries *ParamM;
     QScatterSeries *ParamT;
     QScatterSeries *ParamN;
 
-    std::vector<int> peakDistances;
-    std::vector<int> poincareX;
-    std::vector<int> poincareY;
-    std::vector<int> identityLine;
-    std::vector<int> histogram;
-    std::vector<double> binBorders;
-    std::vector<int> maxBinSize;
-    std::vector<int> maxBinNumber;
-    std::vector<double> xVectorMT;
-    std::vector<double> yVectorMT;
-    std::vector<double> xVectorMN;
-    std::vector<double> yVectorMN;
-    std::vector<double> xVectorTN;
-    std::vector<double> yVectorTN;
-    std::vector<double> triangularIndex;
-    std::vector<int> indicesSD1;
-    std::vector<int> valuesSD1;
-    std::vector<int> bins;
-    std::vector<int> dupa;
+    int bins;
+    std::vector<float> highBins;
+    std::vector<float> binsCenter;
+    float binsWidth;
     int xT;
     int yT;
     int xM;
@@ -110,8 +54,7 @@ private :
 
     void addSeriesToChart();
 
-
-
-
 };
+
+
 #endif //ECG_ANALYZER_HRV2_CHARTCALLOUT_H
